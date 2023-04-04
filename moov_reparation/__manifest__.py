135b25a374ce9398b46e12d3c40201d4af0d389d
Saumil Gauswami <saumil@lapisinfo.com>
2023-04-04 14:04:57 +0530

    [IMP] updated code for the sequence and sms template

---
+++ moov_reparation/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Reparation Sequence',
+    'version': '16.0.1.0.0',
+    'depends': [
+        'project',
+        'industry_fsm',
+    ],
+    'data': [
+        'data/sequence.xml',
+        'views/task.xml',
+        'views/sms_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'demo': [
+    ],
+}
