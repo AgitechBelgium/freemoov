86c661b6e3ce319343d431069939a274fdfeb6ee
Saumil Gauswami <saumil@lapisinfo.com>
2023-09-22 16:33:40 +0530

    [IMP][16.0]Improved code to change condition of Mark as Done button in field service.

--- mask_as_done_extends/__manifest__.py
+++ mask_as_done_extends/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Mark As Done ',
-    'version': '16.0.1.0.0',
-    'depends': [
-        'timesheet_grid',
-    ],
-    'data': [
-        'views/project_task_views.xml',
-    ],
-    'license': 'LGPL-3',
-    'demo': [
-    ],
+	'name': 'Mark As Done ',
+	'version': '16.0.1.0.1',
+	'depends': [
+		'timesheet_grid', 'industry_fsm_stock'
+	],
+	'data': [
+		'views/project_task_views.xml',
+	],
+	'license': 'LGPL-3',
+	'demo': [
+	],
 }
