cb0336b00869cff9a0057f141a423612d49ef1c7
Bhavin <bhavin.kanani@ensigncode.com>
2023-10-16 13:30:40 +0530

    [UPD] update the manifest for freemovv website

--- website_freemoov/__manifest__.py
+++ website_freemoov/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Website Freemoov',
     'category': 'Website',
-    'version': '16.0.0.0.0',
+    'version': '16.0.1.0.1',
     'summary': 'Website Freemoov',
     'author': '',
     'website' : '',
