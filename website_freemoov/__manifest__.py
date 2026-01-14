0427619641b1756eb48b200b7a054b2370f8f14f
Enzo NAUTÉ <hey@enzonaute.me>
2026-01-15 00:47:27 +0100

    chore: Bump website_freemoov version to force module update on Odoo.sh

--- website_freemoov/__manifest__.py
+++ website_freemoov/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Website Freemoov',
     'category': 'Website',
-    'version': '17.0.0.0.7',
+    'version': '17.0.0.0.8',
     'summary': 'Website Freemoov',
     'author': '',
     'website' : '',
