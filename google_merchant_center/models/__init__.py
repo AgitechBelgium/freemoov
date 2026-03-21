0749b395d221caa99e79728d70a5dccbda66f6fe
Enzo NAUTÉ <hey@enzonaute.me>
2026-03-21 02:50:03 +0100

    Revert "Merge pull request #10 from Agitech-Official/feature/gmc-module"
    This reverts commit 97813c1a527429df86046d884dd07e076cd4d2b6, reversing
    changes made to 3c846be0b774e14bd25a8b3fa38b8669c39ef4c9.

--- google_merchant_center/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config_settings
-from . import product_public_category
-from . import product_template
-from . import google_merchant_log
--- google_merchant_center/services/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import google_merchant_service
